e0d6fbb434914de90fa5c2e60180e84b4080d7f4
GothicXSPB <ZZZerggg@mail.ru>
2023-03-27 14:45:20 +0300

    add Cinema models

---
+++ BookingTickets.Api/Controllers/OutputModels/CinemaOutputModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class CinemaOutputModel
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Address { get; set; }
+	public bool? IsDeleted { get; set; } = false;
+}
--- BookingTickets.Api/Controllers/OutputModels/HallOutputModel.cs
+++ BookingTickets.Api/Controllers/OutputModels/HallOutputModel.cs
@@ -5,5 +5,5 @@ public class HallOutputModel
 	public int Id { get; set; }
 	public int CinemaId { get; set; }
 	public int Number { get; set; }
-	public bool? IsDeleted { get; set; }
+	public bool? IsDeleted { get; set; } = false;
 }
