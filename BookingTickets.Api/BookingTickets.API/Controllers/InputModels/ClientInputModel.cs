f4400abad01b2b12c66335302752a1669bbdd16a
GothicXSPB <ZZZerggg@mail.ru>
2023-03-28 01:10:57 +0300

    add client model

---
+++ BookingTickets.Api/BookingTickets.API/Controllers/InputModels/ClientInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookingTickets.API.Controllers.InputModels
+{
+    public class ClientInputModel
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ BookingTickets.Api/BookingTickets.API/Controllers/OutputModels/ClientOutputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookingTickets.API.Controllers.OutputModels
+{
+    public class ClientOutputModel
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
