37ad8c5f17632c53bdb037faa17453b4be1cb838
GothicXSPB <ZZZerggg@mail.ru>
2023-03-28 18:06:02 +0300

    add user models

---
+++ BookingTickets.Api/BookingTickets.API/Controllers/InputModels/UserInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingTickets.API.Controllers.InputModels
+{
+    public class UserInputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Password { get; set; }
+        public int? CinemaId { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
