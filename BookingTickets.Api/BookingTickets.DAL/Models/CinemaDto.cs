3dc19cb60a2030673ee194df06e2c75bc677696d
Ketrin <leanworld2020@gmail.com>
2023-03-29 14:40:12 +0300

    edit Cinema DTO

--- BookingTickets.Api/BookingTickets.DAL/Models/CinemaDto.cs
+++ BookingTickets.Api/BookingTickets.DAL/Models/CinemaDto.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Address { get; set; }
         public List<UserDto> Employes { get; set; }
+        public List<HallDto> Halls { get; set; }
         public bool IsDeleted { get; set; }
     }
 }
