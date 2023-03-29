bf25bc771fe7547e941f0c9c70942a55d60050a1
Ketrin <leanworld2020@gmail.com>
2023-03-29 14:53:21 +0300

    create Repo+Interface and edit HallDto

---
+++ BookingTickets.Api/BookingTickets.DAL/HallRepository.cs
@@ -0,0 +1,28 @@
+﻿using BookingTickets.DAL.Interfaces;
+using BookingTickets.DAL.Models;
+
+namespace BookingTickets.DAL
+{
+    public class HallRepository : IHallRepository
+    {
+        public int CreateHall(HallDto hall)
+        {
+            return hall.Id;
+        }
+
+        public void UpdateHall(HallDto hall)
+        {
+
+        }
+
+        public void DeleteHall(int idHall)
+        {
+
+        }
+
+        public List<SeatDto> GetAllSeatsByHallId(int idHall)
+        {
+            return new List<SeatDto>();
+        }
+    }
+}
