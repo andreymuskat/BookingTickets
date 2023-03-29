a5d09649a4ebddecd7ab48809ffe2749542cec1c
Ketrin <leanworld2020@gmail.com>
2023-03-29 14:19:11 +0300

    edit model API

--- BookingTickets.Api/BookingTickets.API/InputModels/CinemaInputModel.cs
+++ BookingTickets.Api/BookingTickets.API/InputModels/CinemaInputModel.cs
@@ -2,10 +2,7 @@
 {
     public class CinemaInputModel
 	{
-		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Address { get; set; }
-		public bool IsDeleted { get; set; }
-		public List<UserInputModel> Employes { get; set; } = new();
 	}
 }
