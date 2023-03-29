4527a50ac9dfadb76b9da154186d6308bc26b942
GothicXSPB <ZZZerggg@mail.ru>
2023-03-29 11:54:30 +0300

    add automapper

---
+++ BookingTickets.Api/BookingTickets.API/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace BookingTickets.API
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Password { get; set; }
+
+
+
+    }
+}
---
+++ BookingTickets.Api/BookingTickets.BLL/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BookingTickets.DAL.Models;
+
+namespace BookingTickets.BLL
+{
+    public class MapperX
+    {
+        private readonly MapperConfiguration _configuration;
+
+        public MapperX()
+        {
+            _configuration = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.CreateMap<FilmDto, FilmOutputModel>
+                });
+        }
+    }
+}
