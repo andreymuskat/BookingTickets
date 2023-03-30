2d50683bd90a1e3fb99071f20610c9a7c50f5402
GothicXSPB <ZZZerggg@mail.ru>
2023-03-31 00:29:29 +0300

    я полагаю, что 1 метод всё такие работает, но ему пока не хватает базы данных, это ЛУЧШЕ не мерджить, оно ломает всё к херам, но может так и надо

---
+++ BookingTickets.Api/BookingTickets.API/FilmApi.cs
@@ -0,0 +1,26 @@
+﻿using BookingTickets.API.Model.RequestModels;
+using BookingTickets.BLL;
+using BookingTickets.DAL;
+using BookingTickets.DAL.Interfaces;
+
+namespace BookingTickets.API
+{
+    public class FilmApi
+    {
+        private readonly MapperAPI _mapper = new();
+        private readonly MapperBLL _map = new();
+        private readonly IFilmRepository _repository;
+
+        public FilmApi(IFilmRepository? repository = null)
+        {
+            _repository = repository ?? new FilmRepository();
+        }
+        public List<FilmResponseModel> GetAllFilmByCinema(CinemaRequestModel cinema)
+        {
+            var res = _map.MapCinemaBLLToCinemaDto(_mapper.MapCinemaRequestModelToCinemaBLL(cinema));
+            
+            return _mapper.MapListFilmBLLToListFilmResponseModel(
+                _map.MapListFilmDtoToListFilmBLL(_repository.GetAllFilmByCinema(res)));
+        }
+    }
+}
--- BookingTickets.Api/BookingTickets.BLL/Mapper.cs
+++ BookingTickets.Api/BookingTickets.BLL/Mapper.cs
@@ -5,26 +5,36 @@ using BookingTickets.DAL.Models;
 
 namespace BookingTickets.BLL
 {
-    public class MapperX
+    public class MapperBLL
     {
         private readonly MapperConfiguration _configuration;
 
-        public MapperX()
+        public MapperBLL()
         {
             _configuration = new MapperConfiguration(
                 cfg =>
                 {
-                    cfg.CreateMap<FilmDto, FilmResponseModel>();
+                    cfg.CreateMap<FilmDto, FilmBLL>();
+                    //cfg.CreateMap<FilmDto, FilmResponseModel>();
+                    //cfg.CreateMap<FilmResponseModel, FilmDto>();
                     cfg.CreateMap<CinemaInputModel, CinemaDto>();
                     cfg.CreateMap<HallDto, FullHallOutputModel>();
                     cfg.CreateMap<HallInputModel, HallDto>();
+                    //cfg.CreateMap<FilmRequestModel, FilmDto>();
+                    cfg.CreateMap<CinemaBLL, CinemaDto>();
                 });
         }
 
-        public FilmResponseModel MapFilmDtoToFilmoutputModel(FilmDto film)
-        { 
-            return _configuration.CreateMapper().Map<FilmResponseModel>(film);
+        public List<FilmBLL> MapListFilmDtoToListFilmBLL(List<FilmDto> film)
+        {
+            return _configuration.CreateMapper().Map<List<FilmBLL>>(film);
         }
+
+
+        //public FilmResponseModel MapFilmDtoToFilmResponseModel(FilmDto film)
+        //{ 
+        //    return _configuration.CreateMapper().Map<FilmResponseModel>(film);
+        //}
 
         public CinemaDto MapCinemaInputModelToCinemaDto(CinemaInputModel cinema)
         {
@@ -39,6 +49,16 @@ namespace BookingTickets.BLL
         public HallDto MapHallInputModelToHallDto(HallInputModel hall)
         {
             return _configuration.CreateMapper().Map<HallDto>(hall);
+        }
+
+        //public FilmRequestModel MapFilmDtoToFilmRequestModel(FilmDto film)
+        //{
+        //    return _configuration.CreateMapper().Map<FilmRequestModel>(film);
+        //}
+
+        public CinemaDto MapCinemaBLLToCinemaDto(CinemaBLL cinema)
+        {
+            return _configuration.CreateMapper().Map<CinemaDto>(cinema);
         }
     }
 }
