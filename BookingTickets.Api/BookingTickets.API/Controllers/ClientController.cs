061e4a2ec4cd5cec4ace57ae328935efabf93664
GothicXSPB <ZZZerggg@mail.ru>
2023-04-05 00:56:28 +0300

    fix

--- BookingTickets.Api/BookingTickets.API/Controllers/ClientController.cs
+++ BookingTickets.Api/BookingTickets.API/Controllers/ClientController.cs
@@ -16,36 +16,35 @@ namespace BookingTickets.API.Controllers
             film = new FilmApi();
         }
 
-        [HttpGet("{GetFilmByCinema}", Name = "GetAllFilmByCinema")]
+        [HttpGet( Name = "GetAllFilmByCinema")]
 
         public IActionResult GetAllFilmByCinema(CinemaRequestModel cinema)
         {
-            var result = film.GetAllFilmByCinema(cinema);
-            return Ok(result);
+            return Ok(film.GetAllFilmByCinema(cinema));
         }
 
-        [HttpGet("{GetFilmByDay}", Name = "GetFilmByDay")]
+        [HttpGet( Name = "GetFilmByDay")]
         public IActionResult GetAllFilmByDay(DateTime dataTime)
         {
             var result = film.GetAllFilmByDay(dataTime);
             return Ok(result);
         }
 
-        [HttpGet("{GetAllFilm}", Name = "GetAllFilm")]
+        [HttpGet( Name = "GetAllFilm")]
         public IActionResult GetAllFilm()
         {
             var result = film.GetAllFilm;
             return Ok(result);
         }
 
-        [HttpPost("{AddNewFilm}", Name = "AddNewFilm")]
+        [HttpPost( Name = "AddNewFilm")]
         public IActionResult AddNewFilm(FilmRequestModel model)
         {
             film.AddNewFilm(model);
             return Ok("Success");
         }
 
-        [HttpPost("{UpdateFilm}", Name = "UpdateFilm")]
+        [HttpPost( Name = "UpdateFilm")]
         public IActionResult UpdateFilm(FilmRequestModel model)
         {
             film.UpdateFilm(model);
