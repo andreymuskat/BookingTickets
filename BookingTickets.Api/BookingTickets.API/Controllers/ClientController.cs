acd0e637fd8a9345839c696ab2645f6316447328
GothicXSPB <ZZZerggg@mail.ru>
2023-04-04 17:33:57 +0300

    add methods in controller

--- BookingTickets.Api/BookingTickets.API/Controllers/ClientController.cs
+++ BookingTickets.Api/BookingTickets.API/Controllers/ClientController.cs
@@ -17,11 +17,39 @@ namespace BookingTickets.API.Controllers
         }
 
         [HttpGet("{GetFilmByCinema}", Name = "GetAllFilmByCinema")]
-        
+
         public IActionResult GetAllFilmByCinema(CinemaRequestModel cinema)
         {
             var result = film.GetAllFilmByCinema(cinema);
             return Ok(result);
+        }
+
+        [HttpGet("{GetFilmByDay}", Name = "GetFilmByDay")]
+        public IActionResult GetAllFilmByDay(DateTime dataTime)
+        {
+            var result = film.GetAllFilmByDay(dataTime);
+            return Ok(result);
+        }
+
+        [HttpGet("{GetAllFilm}", Name = "GetAllFilm")]
+        public IActionResult GetAllFilm()
+        {
+            var result = film.GetAllFilm;
+            return Ok(result);
+        }
+
+        [HttpPost("{AddNewFilm}", Name = "AddNewFilm")]
+        public IActionResult AddNewFilm(FilmRequestModel model)
+        {
+            film.AddNewFilm(model);
+            return Ok("Success");
+        }
+
+        [HttpPost("{UpdateFilm}", Name = "UpdateFilm")]
+        public IActionResult UpdateFilm(FilmRequestModel model)
+        {
+            film.UpdateFilm(model);
+            return Ok("Success");
         }
 
     }
