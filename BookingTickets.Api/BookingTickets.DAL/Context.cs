1372a401a74fd72c9588b290e352ebd878e3f3fb
andreymuskat <andrey.strel.2001@gmail.com>
2023-04-05 03:21:02 +0300

    add context

---
+++ BookingTickets.Api/BookingTickets.DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookingTickets.DAL.Models;
+
+namespace BookingTickets.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            //builder.UseSqlServer(sqlConectionString)
+            builder.UseInMemoryDatabase("BookingTicketsDb");
+        }
+
+        public DbSet<HallDto> Hall { get; set; }
+    }
+}
