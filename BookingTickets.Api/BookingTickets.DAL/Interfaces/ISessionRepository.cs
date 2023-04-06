52477d61719e68018cd8e9ffef983ab903118a95
DariaKostrik <dolorez0927@yandex.ru>
2023-04-07 01:53:50 +0300

    Add SessionRepository + new methods for iSessionRepository

--- BookingTickets.Api/BookingTickets.DAL/Interfaces/ISessionRepository.cs
+++ BookingTickets.Api/BookingTickets.DAL/Interfaces/ISessionRepository.cs
@@ -6,6 +6,11 @@ namespace BookingTickets.DAL.Interfaces
     {
         public SessionDto CreateSession(SessionDto session);
         public void UpdateSession(SessionDto session);
+        public List<SessionDto> GetAllSession();
         public void DeleteSession(int idSession);
+        public List<SessionDto> GetAllSessionByFilmId(int idFilm);
+        public List<SessionDto> GetAllSessionByCinemaId(int idCinema);
+        public List<SessionDto> GetAllSessionByDate(DateTime Date);
+
     }
 }
---
+++ BookingTickets.Api/BookingTickets.DAL/SessionRepository.cs
@@ -0,0 +1,50 @@
+﻿using BookingTickets.DAL.Interfaces;
+using BookingTickets.DAL.Models;
+using Core;
+
+namespace BookingTickets.DAL
+{
+    public class SessionRepository: ISessionRepository
+    {
+        private readonly Context _context;
+
+        public SessionRepository (Context context)
+        {
+            _context = context;
+        }
+        public SessionDto CreateSession(SessionDto session)
+        { 
+            _context.Sessions.Add(session);
+            _context.SaveChanges();
+            return session;
+        }
+
+        public List<SessionDto> GetAllSession()
+        {
+            return new List<SessionDto>();
+        }
+
+        public void UpdateSession(SessionDto session)
+        {
+
+        }
+
+        public List<SessionDto> GetAllSessionByFilmId(int idFilm)
+        {
+            return new List<SessionDto>();
+        }
+
+        public List<SessionDto> GetAllSessionByCinemaId(int idCinema)
+        {
+            return new List<SessionDto>();
+        }
+        public List<SessionDto> GetAllSessionByDate(DateTime Date)
+        {
+            return new List<SessionDto>();
+        }
+        public void DeleteSession(int idSession)
+        {
+
+        }
+    }
+}
