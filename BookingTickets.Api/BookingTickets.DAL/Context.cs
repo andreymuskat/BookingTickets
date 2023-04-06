200e1d2cef2458dc27407c35a9c2a92d319ae08c
GothicXSPB <ZZZerggg@mail.ru>
2023-04-06 16:12:11 +0300

    create all context

--- BookingTickets.Api/BookingTickets.DAL/Context.cs
+++ BookingTickets.Api/BookingTickets.DAL/Context.cs
@@ -14,5 +14,11 @@ namespace BookingTickets.DAL
         public DbSet<HallDto> Halls { get; set; }
 
         public DbSet<FilmDto> Films { get; set; }
+
+        public DbSet<CinemaDto> Cinemas { get; set; }
+        public DbSet<OrderDto> Orders { get; set; }
+        public DbSet<SeatDto> Seats { get; set; }
+        public DbSet<SessionDto> Sessions { get; set; }
+        public DbSet<UserDto> Users { get; set; }
     }
 }
