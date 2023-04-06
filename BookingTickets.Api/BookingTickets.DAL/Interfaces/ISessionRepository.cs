be27294d10abb631b180bed4c034e1a08a28beb5
DariaKostrik <dolorez0927@yandex.ru>
2023-04-07 01:34:47 +0300

    Add ISessionRepository

---
+++ BookingTickets.Api/BookingTickets.DAL/Interfaces/ISessionRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookingTickets.DAL.Models;
+
+namespace BookingTickets.DAL.Interfaces
+{
+    public interface ISessionRepository
+    {
+        public SessionDto CreateSession(SessionDto session);
+        public void UpdateSession(SessionDto session);
+        public void DeleteSession(int idSession);
+    }
+}
