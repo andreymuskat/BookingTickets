333276ec9e17573033a95fae445c9a080d53120d
GothicXSPB <ZZZerggg@mail.ru>
2023-04-11 13:00:19 +0300

    fix

--- BookingTickets.Api/BookingTickets.BLL/MapperBLL.cs
+++ BookingTickets.Api/BookingTickets.BLL/MapperBLL.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:BookingTickets.Api/BookingTickets.BLL/Mapper.cs
+
 using AutoMapper;
 using BookingTickets.BLL.Models.InputModels;
 using BookingTickets.BLL.Models.OutputModels;
@@ -62,66 +62,3 @@ namespace BookingTickets.BLL
         }
     }
 }
-=======
-﻿using AutoMapper;
-using BookingTickets.BLL.Models.InputModels;
-using BookingTickets.BLL.Models.OutputModels;
-using BookingTickets.DAL.Models;
-
-namespace BookingTickets.BLL
-{
-    public class MapperBLL
-    {
-        private readonly MapperConfiguration _configuration;
-
-        public MapperBLL()
-        {
-            _configuration = new MapperConfiguration(
-                cfg =>
-                {
-                    cfg.CreateMap<FilmDto, FilmBLL>();                    
-                    cfg.CreateMap<FilmBLL, FilmDto>();                    
-                    cfg.CreateMap<CinemaInputModel, CinemaDto>();
-                    cfg.CreateMap<HallDto, FullHallOutputModel>();
-                    cfg.CreateMap<HallInputModel, HallDto>();                    
-                    cfg.CreateMap<CinemaBLL, CinemaDto>();
-                });
-        }
-
-        public List<FilmBLL> MapListFilmDtoToListFilmBLL(List<FilmDto> film)
-        {
-            return _configuration.CreateMapper().Map<List<FilmBLL>>(film);
-        }
-
-        public FilmBLL MapFilmDtoToFilmBLL(FilmDto film)
-        {
-            return _configuration.CreateMapper().Map<FilmBLL>(film);
-        }
-
-        public FilmDto MapFilmBLLToFilmDto(FilmBLL film)
-        {
-            return _configuration.CreateMapper().Map<FilmDto>(film);
-        }
-
-        public CinemaDto MapCinemaInputModelToCinemaDto(CinemaInputModel cinema)
-        {
-            return _configuration.CreateMapper().Map<CinemaDto>(cinema);
-        }
-
-        public FullHallOutputModel MapHallDtoToFullHallOutputModel(HallDto hall)
-        {
-            return _configuration.CreateMapper().Map<FullHallOutputModel>(hall);
-        }
-
-        public HallDto MapHallInputModelToHallDto(HallInputModel hall)
-        {
-            return _configuration.CreateMapper().Map<HallDto>(hall);
-        }
-
-        public CinemaDto MapCinemaBLLToCinemaDto(CinemaBLL cinema)
-        {
-            return _configuration.CreateMapper().Map<CinemaDto>(cinema);
-        }
-    }
-}
->>>>>>> 5.6+3AddControllerAndMapperForFilm:BookingTickets.Api/BookingTickets.BLL/MapperBLL.cs
