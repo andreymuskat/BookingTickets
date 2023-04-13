061841b329e8c7fdc355b13999be2ac164b57118
GothicXSPB <ZZZerggg@mail.ru>
2023-04-13 23:15:34 +0300

    fix

---
+++ BookingTickets.Api/BookingTickets.BLL/Roles/MainAdmin.cs
@@ -0,0 +1,38 @@
+﻿using BookingTickets.BLL.Models;
+using BookingTickets.BLL.NewFolder;
+using BookingTickets.DAL.Interfaces;
+
+namespace BookingTickets.BLL.Roles
+{
+    public class MainAdmin : IMainAdmin
+    {
+        private MapperBLL _instanceMapperBll = MapperBLL.getInstance();
+        private IFilmRepository _filmRepository;
+
+        public MainAdmin(IFilmRepository repository)
+        {
+            _filmRepository = repository;
+        }
+
+        public void AddNewFilm(FilmBLL newFilm)
+        {
+            var filmDto = _instanceMapperBll.MapFilmInputModelToFilmDto(newFilm);
+            var filmByName = _filmRepository.GetFilmByName(filmDto.Name);
+            if (filmByName == null)
+            {
+                _filmRepository.CreateFilm(filmDto);
+            }
+            else
+            {
+                throw new Exception("Такой фильм уже есть в базе!");
+            }
+        }
+
+        public FilmBLL GetFilmByName(string name)
+        {
+            var res = _filmRepository.GetFilmByName(name);
+
+            return _instanceMapperBll.MapFilmDtoToFilmBLL(res);
+        }
+    }
+}
