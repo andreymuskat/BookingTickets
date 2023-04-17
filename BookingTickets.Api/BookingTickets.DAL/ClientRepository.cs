d84403aec91c9069b07fd42d0dd2fd8f392fe8e6
andreymuskat <andrey.strel.2001@gmail.com>
2023-04-17 14:59:06 +0300

    add User rep

---
+++ BookingTickets.Api/BookingTickets.DAL/ClientRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookingTickets.DAL.Interfaces;
+using BookingTickets.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingTickets.DAL
+{
+    public class UserRepository: IUserRepository
+    {
+        private static Context _context;
+
+        public UserDto AddNewUser(UserDto userDto)
+        {
+            _context.Users.Add(userDto);
+
+            _context.SaveChanges();
+
+            return new UserDto();
+        }
+    }
+}
---
+++ BookingTickets.Api/BookingTickets.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookingTickets.DAL.Models;
+
+namespace BookingTickets.DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        public UserDto AddNewUser(UserDto userDto);
+    }
+}
