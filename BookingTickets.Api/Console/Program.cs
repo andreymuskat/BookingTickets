5673c671264340cf7d04a1a75bb2fb96dd79669f
Ketrin <leanworld2020@gmail.com>
2023-04-19 17:50:37 +0300

    console project for test

---
+++ BookingTickets.Api/Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using BookingTickets.DAL;
+using System.Collections;
+
+SessionRepository SessionRepository= new SessionRepository();
+
+
+var ss  = SessionRepository.GetAllSessionByCinemaId(1);
+
+Console.WriteLine(ss);
