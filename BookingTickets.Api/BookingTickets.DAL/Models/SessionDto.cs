dff2762bcec38528d08626e9687f93b2d4b74b09
GothicXSPB <ZZZerggg@mail.ru>
2023-04-21 13:34:09 +0300

    ы

--- BookingTickets.Api/BookingTickets.DAL/Models/SessionDto.cs
+++ BookingTickets.Api/BookingTickets.DAL/Models/SessionDto.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
 using BookingTickets.DAL.Models;
 
 public class SessionDto
