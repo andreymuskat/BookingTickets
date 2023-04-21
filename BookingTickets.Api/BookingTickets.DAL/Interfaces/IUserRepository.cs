326de6f6660fde2c9bd99d464a03a91773a6c859
andreymuskat <andrey.strel.2001@gmail.com>
2023-04-21 12:33:11 +0300

    add user interface

---
+++ BookingTickets.Api/BookingTickets.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingTickets.DAL.Models;
+
+namespace BookingTickets.DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        public List<UserDto> GetAllUsers();
+
+        public UserDto GetUserById(int id);
+
+        public void CreateCashier(UserDto cashier);
+    }
+}
