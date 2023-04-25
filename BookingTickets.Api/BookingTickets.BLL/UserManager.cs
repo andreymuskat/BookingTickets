121cf3c065e1840886252b33dd0c5d344bb1d0fe
andreymuskat <andrey.strel.2001@gmail.com>
2023-04-25 13:51:32 +0300

    change user manager

--- BookingTickets.Api/BookingTickets.BLL/UserManager.cs
+++ BookingTickets.Api/BookingTickets.BLL/UserManager.cs
@@ -1,6 +1,37 @@
-﻿namespace BookingTickets.BLL
+﻿using BookingTickets.BLL.Models;
+using BookingTickets.DAL.Interfaces;
+using BookingTickets.DAL;
+
+namespace BookingTickets.BLL
 {
     public class UserManager
     {
+        private MapperBLL _instanceMapperBll = MapperBLL.getInstance();
+        private readonly IUserRepository _userRepository;
+
+        public UserManager()
+        {
+            _userRepository = new UserRepository();
+
+        }
+
+        public List<UserBLL> GetAllUsers()
+        {
+            var allUsers = _userRepository.GetAllUsers();
+
+            return _instanceMapperBll.MapListUserDtoToListUserBLL(allUsers);
+        }
+        
+        public List<UserBLL> GetAllCashiers()
+        {
+            var allUsers = _userRepository.GetAllCashiers();
+
+            return _instanceMapperBll.MapListUserDtoToListUserBLL(allUsers);
+        }
+
+        public UserBLL AddNewUser(UserBLL user)
+        {
+
+        }
     }
 }
