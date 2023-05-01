a44a78a54963ff34869dbf3c6fdecb061f936be5
DariaKostrik <dolorez0927@yandex.ru>
2023-05-01 14:36:47 +0300

    add mistakes to Cashier api bll

---
+++ BookingTickets.Api/BookingTickets.BLL/CustomException/OrderException.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+
+namespace BookingTickets.BLL.CustomException
+{
+    public class OrderException: Exception
+    {
+        private CodeException _errorCode;
+        public CodeException ErrorCode { get { return _errorCode; } }
+
+        public OrderException(int code)
+            : base()
+        {
+            _errorCode = (CodeException)(code);
+        }
+    }
+}
