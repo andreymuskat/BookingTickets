b89cd468d358cd54caf0b32c0ac0527e4c6538ff
andreymuskat <andrey.strel.2001@gmail.com>
2023-05-03 19:46:30 +0300

    fix error

--- BookingTickets.Api/BookingTickets.BLL/InterfacesBll/Service_Interfaces/IAdminService.cs
+++ BookingTickets.Api/BookingTickets.BLL/InterfacesBll/Service_Interfaces/IAdminService.cs
@@ -1,19 +1,24 @@
 ﻿using BookingTickets.BLL.Models;
 using BookingTickets.BLL.Models.InputModel.All_Session_InputModel;
 using BookingTickets.BLL.Models.InputModel.All_Statistics_InputModels;
-using BookingTickets.BLL.Models.InputModel.All_User_InputModel;
 using BookingTickets.BLL.Models.OutputModel.All_Statistics_OutputModels;
 
 namespace BookingTickets.BLL.InterfacesBll.Service_Interfaces
 {
     public interface IAdminService
     {
-        UserBLL CreateNewCashier(CreateCashierInputModel newUser);
+        void CreateNewCashier(int cashierId);
+
         void CreateSession(CreateSessionInputModel session, int cinemaId, int userId);
-        void DeleteCashierById(int id);
+
+        void DeleteCashierById(int idCashier, int adminCinemaId);
+
         void DeleteSession(int sessionId);
-        List<UserBLL> GetAllCashiers();
+
         List<UserBLL> GetAllUsers();
+
+        public List<UserBLL> GetAllCashiers(int adminCinemaId);
+
         StatisticsFilm_OutputModels GetStatisticsByFilm(StatisticsFilm_InputModels infoForStatic, int cinemaId);
     }
 }
